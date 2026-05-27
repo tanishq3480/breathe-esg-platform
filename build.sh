5b1036fa7af3a7898998d7ee74395652a50e955f
Tanishq Sharma <lipitanishq@gmail.com>
2026-05-27 15:58:52 +0530

    fix: whitenoise static files, superuser auto-creation on deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
-
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+echo "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='tanishq').exists():
+    User.objects.create_superuser('tanishq', 'tanishq@breatheesg.com', 'p@ssw0rd3480')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+" | python manage.py shell
